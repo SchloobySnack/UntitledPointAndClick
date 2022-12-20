0dcaa2328c44c34fd41885ded25ea25656835149
Charles Schlue <schluecharlesr@johndeere.com>
2022-12-20 01:54:01 -0600

    Added a simple pause screen & main menu button

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -2,12 +2,18 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.AI;
+using UnityEngine.SceneManagement;
 public class GameManager : MonoBehaviour 
 {
     public NavMeshAgent playerNavMeshAgent;
     
     // Declare a static instance of the GameManager class
     public static GameManager instance;
+
+    // Declare a public field for the UI canvas
+    public Canvas uiCanvas;
+    // Declare a flag to track the pause state
+    bool isPaused = false;
 
     // Start is called before the first frame update
     void Start()
@@ -19,6 +25,12 @@ public class GameManager : MonoBehaviour
         else
         {
             instance = this;
+            // Access the UI canvas from the GameManager
+                    // Find the Canvas GameObject
+            GameObject canvasObject = GameObject.Find("Canvas");
+
+            // Get the Canvas component from the Canvas GameObject
+            uiCanvas = canvasObject.GetComponent<Canvas>();
             DontDestroyOnLoad(gameObject);
         }
     }
@@ -38,6 +50,28 @@ public class GameManager : MonoBehaviour
                 playerNavMeshAgent.SetDestination(targetLocation);
             }
         }
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            // Fire the Pause function
+            Pause();
+        }
+    }
+
+    public void LoadLevel(string sceneName)
+    {
+        SceneManager.LoadScene(sceneName);
+    }
+    // Pause function
+    void Pause()
+    {
+        // Toggle the pause state
+        isPaused = !isPaused;
+
+        // Pause or unpause the game based on the pause state
+        Time.timeScale = isPaused ? 0 : 1;
+
+        // Show or hide the pause menu based on the pause state
+        uiCanvas.gameObject.SetActive(isPaused);
     }
 
 }
