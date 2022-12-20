2f90102b5968384513eebefac4f3f3fb53ce01b5
Charles Schlue <schluecharlesr@johndeere.com>
2022-12-19 22:48:05 -0600

    Added NavMesh move on click

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,18 +1,26 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
-public class GameManager : MonoBehaviour
+using UnityEngine.AI;
+public class GameManager : MonoBehaviour 
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
+    public NavMeshAgent playerNavMeshAgent;
+    
     void Update()
     {
-        
+        if (Input.GetMouseButtonDown(0))
+        {
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            RaycastHit hit;
+            if (Physics.Raycast(ray, out hit))
+            {
+                Vector3 targetLocation = hit.point;
+
+                // Set the target position for the nav mesh agent
+                playerNavMeshAgent.SetDestination(targetLocation);
+            }
+        }
     }
+
 }
+
