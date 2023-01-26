0ef456c2d3325c061da35f844eb05d55de160118
Charles Schlue <schluecharlesr@johndeere.com>
2023-01-26 01:21:31 -0600

    Migrated ai from gamemanager to character class

--- Assets/Scripts/CharacterController/Character.cs
+++ Assets/Scripts/CharacterController/Character.cs
@@ -1,16 +1,16 @@
 using System;
 using UnityEngine;
-
+using UnityEngine.AI;
 namespace HeyAlexi
 {
-    [RequireComponent(typeof(UnityEngine.AI.NavMeshAgent))]
     public class Character : MonoBehaviour
     {
-        Animator anim;
-        UnityEngine.AI.NavMeshAgent agent;
+        private Animator anim;
+        private NavMeshAgent agent;
+        private NavMeshPath playerPath;
         Vector2 smoothDeltaPosition = Vector2.zero;
         Vector2 velocity = Vector2.zero;
-        private CurrentState currentState;
+        public CurrentState currentState;
 
         void Start()
         {
@@ -26,31 +26,31 @@ namespace HeyAlexi
         void Update()
         {
             currentState.State.Update();
-            Vector3 worldDeltaPosition = agent.destination - transform.position;
+            // Vector3 worldDeltaPosition = agent.destination - transform.position;
 
-            // Map 'worldDeltaPosition' to local space
-            float dx = Vector3.Dot(transform.right, worldDeltaPosition);
-            float dy = Vector3.Dot(transform.forward, worldDeltaPosition);
-            Vector2 deltaPosition = new Vector2(dx, dy);
+            // // Map 'worldDeltaPosition' to local space
+            // float dx = Vector3.Dot(transform.right, worldDeltaPosition);
+            // float dy = Vector3.Dot(transform.forward, worldDeltaPosition);
+            // Vector2 deltaPosition = new Vector2(dx, dy);
 
-            // Low-pass filter the deltaMove
-            float smooth = Mathf.Min(1.0f, Time.deltaTime / 0.15f);
-            smoothDeltaPosition = Vector2.Lerp(smoothDeltaPosition, deltaPosition, smooth);
+            // // Low-pass filter the deltaMove
+            // float smooth = Mathf.Min(1.0f, Time.deltaTime / 0.15f);
+            // smoothDeltaPosition = Vector2.Lerp(smoothDeltaPosition, deltaPosition, smooth);
 
-            // Update velocity if delta time is safe
-            if (Time.deltaTime > 1e-5f)
-                velocity = smoothDeltaPosition / Time.deltaTime;
+            // // Update velocity if delta time is safe
+            // if (Time.deltaTime > 1e-5f)
+            //     velocity = smoothDeltaPosition / Time.deltaTime;
 
-            bool shouldMove = velocity.magnitude > 0.01f && agent.remainingDistance > agent.radius;
+            // bool shouldMove = velocity.magnitude > 0.01f && agent.remainingDistance > agent.radius;
 
-            // Update animation parameters
-            anim.SetBool("move", shouldMove);
-            anim.SetFloat("velx", velocity.x);
-            anim.SetFloat("vely", velocity.y);
+            // // Update animation parameters
+            // anim.SetBool("move", shouldMove);
+            // anim.SetFloat("velx", velocity.x);
+            // anim.SetFloat("vely", velocity.y);
 
-            LookAt lookAt = GetComponent<LookAt>();
-            if (lookAt)
-                lookAt.lookAtTargetPosition = agent.steeringTarget + transform.forward;
+            // LookAt lookAt = GetComponent<LookAt>();
+            // if (lookAt)
+            //     lookAt.lookAtTargetPosition = agent.steeringTarget + transform.forward;
 
             //		// Pull character towards agent
             //		if (worldDeltaPosition.magnitude > agent.radius)
@@ -67,14 +67,46 @@ namespace HeyAlexi
             //		transform.position = agent.nextPosition;
 
             // Update position based on animation movement using navigation surface height
-            Vector3 position = anim.rootPosition;
-            position.y = agent.nextPosition.y;
-            transform.position = position;
+            // Vector3 position = anim.rootPosition;
+            // position.y = agent.nextPosition.y;
+            // transform.position = position;
         }
+
+        // Transform target, Vector3 targetPosition
+        public void NavToTarget(RaycastHit hit)
+        {
+            playerPath ??= agent.path;
+            Vector3 targetPosition = hit.point;
+            if (currentState.State is Idle )
+            {
+                agent.CalculatePath(targetPosition, playerPath);
+                if (!(agent.path.status == NavMeshPathStatus.PathComplete))
+                {
+                    agent.ResetPath();
+                    return;
+                }
+                agent.SetPath(playerPath);
+            }
+        }
+
+        private bool IsInteractable(GameObject gameObject)
+        {
+            if (gameObject.CompareTag("Interactable"))
+            {
+                return true;
+            }
+            return false;
+        }
+
     }
 
     public abstract class State
-    {
+    {   
+        public State()
+        {
+
+        }
+
         public abstract void Enter();
         public abstract void Update();
         public abstract void Exit();
@@ -112,8 +144,11 @@ namespace HeyAlexi
 
     public class Idle : State
     {
+        public string name = "Idle";
+
         public override void Enter()
         {
+            
             return;
         }
         public override void Update()
--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,7 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.AI;
 using UnityEngine.SceneManagement;
 using TMPro;
 
@@ -9,21 +8,15 @@ namespace HeyAlexi
 {
     public class GameManager : MonoBehaviour
     {
-        public NavMeshAgent playerNavMeshAgent;
-        public GameObject player;
-        public bool debug;
+        [SerializeField] private Character _playerPrefab;
+        private Character player;
         public TextMeshProUGUI innerThought;
         // Declare a static instance of the GameManager class
         public static GameManager instance;
-
         // Declare a public field for the UI canvas
         public Transform uiCanvas;
         // Declare a flag to track the pause state
         bool isPaused = false;
-        public IEnumerator task;
-        private NavMeshPath PlayerPath;
-
-        LineRenderer lineRenderer;
 
         // Start is called before the first frame update
         void Start()
@@ -41,11 +34,9 @@ namespace HeyAlexi
 
                 // Get the Canvas component from the Canvas GameObject
                 uiCanvas = canvasObject.transform.Find("PauseMenu");
-                player = playerNavMeshAgent.gameObject;
+                player = Instantiate(_playerPrefab);
                 DontDestroyOnLoad(gameObject);
             }
-
-            lineRenderer = GetComponent<LineRenderer>();
         }
 
 
@@ -54,7 +45,8 @@ namespace HeyAlexi
             if (Input.GetMouseButtonDown(0))
             {
                 // Player clicked so move or interact with something
-                PlayerAction(instance.task);
+                // PlayerAction(instance.task);
+                player.NavToTarget(GetMouseClickTarget());
             }
             if (Input.GetKeyDown(KeyCode.Escape))
             {
@@ -62,112 +54,47 @@ namespace HeyAlexi
                 Pause();
             }
 
-            if (debug)
-            {
-                // Some debug stuff, mainly draws a line showing the current path.
-                if (!(PlayerPath == null))
-                {
-                    Vector3[] waypoints = PlayerPath.corners;
+            // if (debug)
+            // {
+            //     // Some debug stuff, mainly draws a line showing the current path.
+            //     if (!(player.agent.PlayerPath == null))
+            //     {
+            //         Vector3[] waypoints = PlayerPath.corners;
 
 
-                    if (waypoints.Length > 0)
-                    {
-                        // Set the positions of the LineRenderer to the array of waypoints
-                        lineRenderer.positionCount = waypoints.Length;
-                        lineRenderer.SetPositions(waypoints);
-                    }
-                    else
-                    {
-                        // If the agent doesn't have a path, hide the LineRenderer
-                        lineRenderer.positionCount = 0;
-                    }
-                }
+            //         if (waypoints.Length > 0)
+            //         {
+            //             // Set the positions of the LineRenderer to the array of waypoints
+            //             lineRenderer.positionCount = waypoints.Length;
+            //             lineRenderer.SetPositions(waypoints);
+            //         }
+            //         else
+            //         {
+            //             // If the agent doesn't have a path, hide the LineRenderer
+            //             lineRenderer.positionCount = 0;
+            //         }
+            //     }
 
-            }
+            // }
 
-            PlayerPath ??= playerNavMeshAgent.path;
+            // PlayerPath ??= playerNavMeshAgent.path;
         }
 
-        private void PlayerAction(IEnumerator task)
-        {
-            if (ReadyForTask(task))
-            {
-                GetMouseClickTarget();
-            }
-            else
-            {
-                playerNavMeshAgent.ResetPath();
-                StopNavMeshAgent();
-                GameManager.instance.task = null;
-                StopCoroutine(task);
-                GetMouseClickTarget();
-                return;
-            }
-        }
-
-        private void GetMouseClickTarget()
+        private RaycastHit GetMouseClickTarget()
         {
             Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
             if (Physics.Raycast(ray, out RaycastHit hit))
             {
-                StartNewTask(hit);
+                return hit;
             }
+            return new RaycastHit();
         }
-        private void StartNewTask(RaycastHit hit)
-        {
-            instance.task = Task(hit);
-            StartCoroutine(instance.task);
-        }
-
-        private bool IsInteractable(GameObject gameObject)
-        {
-            if (gameObject.CompareTag("Interactable"))
-            {
-                return true;
-            }
-            return false;
-
-        }
-        private bool ReadyForTask(IEnumerator task)
-        {
-            if (task is null)
-            {
-                return true;
-            }
-            return false;
-
-        }
-
-        // Temporary fix to stop agent from sliding when a new task is assigned
-        private void StopNavMeshAgent()
-        {
-            playerNavMeshAgent.enabled = false;
-            playerNavMeshAgent.enabled = true;
-        }
-        private void StopTask(IEnumerator task)
-        {
-            if (task is not null)
-            {
-                StopCoroutine(task);
-            }
-        }
-
 
         public void LoadLevel(string sceneName)
         {
             SceneManager.LoadScene(sceneName);
         }
 
-        public void NavToTarget(Transform target, Vector3 targetPosition)
-        {
-            playerNavMeshAgent.CalculatePath(targetPosition, PlayerPath);
-            if (!(PlayerPath.status == NavMeshPathStatus.PathComplete))
-            {
-                PlayerPath = null;
-                return;
-            }
-            playerNavMeshAgent.SetPath(PlayerPath);
-        }
         // Pause function
         void Pause()
         {
@@ -257,39 +184,39 @@ namespace HeyAlexi
             player.transform.rotation = Quaternion.Slerp(player.transform.rotation, targetRotation, rotationSpeed * Time.deltaTime);
         }        
 
-        IEnumerator Task(RaycastHit hit)
-        {
-            while(!(IsFacingTarget(hit.transform)))
-            {
-                RotateTowardsTarget(hit.transform);
-                yield return null;
-            }
+        // IEnumerator Task(RaycastHit hit)
+        // {
+        //     while(!(IsFacingTarget(hit.transform)))
+        //     {
+        //         RotateTowardsTarget(hit.transform);
+        //         yield return null;
+        //     }
 
-            if (IsInteractable(hit.transform.gameObject))
-            {
-                hit.transform.gameObject.GetComponent<Interactable>().Interact(hit);
-                yield break;
-            }
+        //     if (IsInteractable(hit.transform.gameObject))
+        //     {
+        //         hit.transform.gameObject.GetComponent<Interactable>().Interact(hit);
+        //         yield break;
+        //     }
 
-            NavToTarget(hit.transform, hit.point);
+        //     NavToTarget(hit.transform, hit.point);
 
-            while(!(GameManager.instance.task == null))
-            {
-                if (!playerNavMeshAgent.pathPending)
-                {
+        //     while(!(GameManager.instance.task == null))
+        //     {
+        //         if (!playerNavMeshAgent.pathPending)
+        //         {
                     
-                    if (playerNavMeshAgent.remainingDistance <= playerNavMeshAgent.stoppingDistance)
-                    {
-                        if (!playerNavMeshAgent.hasPath || playerNavMeshAgent.velocity.sqrMagnitude == 0f)
-                        {
-                            instance.task = null;
-                            yield break;
-                        }
-                    }
-                }
-                yield return null;
-            }
-        }
+        //             if (playerNavMeshAgent.remainingDistance <= playerNavMeshAgent.stoppingDistance)
+        //             {
+        //                 if (!playerNavMeshAgent.hasPath || playerNavMeshAgent.velocity.sqrMagnitude == 0f)
+        //                 {
+        //                     instance.task = null;
+        //                     yield break;
+        //                 }
+        //             }
+        //         }
+        //         yield return null;
+        //     }
+        // }
 
     }
 }
